a3578b0808f2ed435f18ea6db7c89460e64ca07f
Adelin <filimon.adelin24@gmail.com>
2021-04-05 23:48:01 +0300

    add Spring Boot Project

---
+++ src/test/java/com/gmail/filimon24/adelin/labactivitytracker/LabActivityTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.filimon24.adelin.labactivitytracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabActivityTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
