7914686a40f280a04bbce05add26731bf1802705
Adelin <filimon.adelin24@gmail.com>
2021-04-05 23:55:21 +0300

    Add Persistence Layer

---
+++ src/main/java/com/gmail/filimon24/adelin/labactivitytracker/persistence/TeacherEntity.java
@@ -0,0 +1,20 @@
+package com.gmail.filimon24.adelin.labactivitytracker.persistence;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class TeacherEntity {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
