576576e7116354ecab32f6a51f98e696bdec511f
Adelin <filimon.adelin24@gmail.com>
2021-04-09 16:11:08 +0300

    Add findByEmail query

--- src/main/java/com/gmail/filimon24/adelin/labactivitytracker/persistence/TeacherRepository.java
+++ src/main/java/com/gmail/filimon24/adelin/labactivitytracker/persistence/TeacherRepository.java
@@ -2,8 +2,12 @@ package com.gmail.filimon24.adelin.labactivitytracker.persistence;
 
 import com.gmail.filimon24.adelin.labactivitytracker.persistence.model.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TeacherRepository extends JpaRepository<Teacher, Long> {
+
+    @Query("SELECT t FROM Teacher t WHERE t.email = ?1")
+    Teacher findByEmail(String email);
 }
