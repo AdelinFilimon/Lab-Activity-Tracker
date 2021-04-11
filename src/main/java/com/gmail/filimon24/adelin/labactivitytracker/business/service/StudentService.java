82da09f91d1e6788d0c135aa321db53b68f17e16
Adelin <filimon.adelin24@gmail.com>
2021-04-11 10:00:37 +0300

    Update Services

--- src/main/java/com/gmail/filimon24/adelin/labactivitytracker/business/service/StudentService.java
+++ src/main/java/com/gmail/filimon24/adelin/labactivitytracker/business/service/StudentService.java
@@ -1,7 +1,59 @@
 package com.gmail.filimon24.adelin.labactivitytracker.business.service;
 
+import com.gmail.filimon24.adelin.labactivitytracker.business.exception.FieldType;
+import com.gmail.filimon24.adelin.labactivitytracker.business.exception.InvalidFieldException;
+import com.gmail.filimon24.adelin.labactivitytracker.business.service.mapper.StudentMapper;
+import com.gmail.filimon24.adelin.labactivitytracker.business.validator.EmailValidator;
+import com.gmail.filimon24.adelin.labactivitytracker.business.validator.GroupValidator;
+import com.gmail.filimon24.adelin.labactivitytracker.business.validator.PasswordValidator;
+import com.gmail.filimon24.adelin.labactivitytracker.model.StudentDto;
+import com.gmail.filimon24.adelin.labactivitytracker.persistence.StudentGroupRepository;
+import com.gmail.filimon24.adelin.labactivitytracker.persistence.StudentRepository;
+import com.gmail.filimon24.adelin.labactivitytracker.persistence.model.Student;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class StudentService {
+
+    private final StudentRepository studentRepository;
+    private final StudentGroupRepository studentGroupRepository;
+
+    private final PasswordValidator passwordValidator;
+    private final GroupValidator groupValidator;
+    private final EmailValidator emailValidator;
+    private final PasswordEncoder passwordEncoder;
+
+    private final StudentMapper studentMapper;
+
+    public StudentDto createStudent(StudentDto studentDto) {
+
+        String email = studentDto.getEmail();
+        String password = studentDto.getPassword();
+        Long groupNumber = studentDto.getStudentGroup().getGroupNumber();
+
+        if (!passwordValidator.isValid(password))
+            throw new InvalidFieldException(FieldType.PASSWORD, password);
+
+        if (!groupValidator.isValid(groupNumber))
+            throw new InvalidFieldException(FieldType.STUDENT_GROUP, groupNumber.toString());
+
+        if (!emailValidator.isValid(email))
+            throw new InvalidFieldException(FieldType.EMAIL, email);
+
+        studentDto.setPassword(passwordEncoder.encode(studentDto.getPassword()));
+
+        Long studentGroupId = studentGroupRepository.findStudentGroupByGroupNumber(groupNumber).getId();
+        studentDto.getStudentGroup().setId(studentGroupId);
+        Student student = studentRepository.save(studentMapper.dtoToDao(studentDto));
+        studentDto.setId(student.getId());
+
+        return studentDto;
+    }
+
+
+
+
 }
--- src/main/java/com/gmail/filimon24/adelin/labactivitytracker/business/service/TeacherService.java
+++ src/main/java/com/gmail/filimon24/adelin/labactivitytracker/business/service/TeacherService.java
@@ -2,15 +2,10 @@ package com.gmail.filimon24.adelin.labactivitytracker.business.service;
 
 import com.gmail.filimon24.adelin.labactivitytracker.CustomApplicationProperties;
 import com.gmail.filimon24.adelin.labactivitytracker.business.service.mapper.TeacherMapper;
-import com.gmail.filimon24.adelin.labactivitytracker.business.service.mapper.TokenMapper;
 import com.gmail.filimon24.adelin.labactivitytracker.model.TeacherDto;
-import com.gmail.filimon24.adelin.labactivitytracker.model.TokenDto;
 import com.gmail.filimon24.adelin.labactivitytracker.persistence.TeacherRepository;
-import com.gmail.filimon24.adelin.labactivitytracker.persistence.TokenRepository;
 import com.gmail.filimon24.adelin.labactivitytracker.persistence.model.Teacher;
-import com.gmail.filimon24.adelin.labactivitytracker.persistence.model.Token;
 import lombok.RequiredArgsConstructor;
-import net.bytebuddy.utility.RandomString;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -20,8 +15,6 @@ public class TeacherService {
 
     private final PasswordEncoder passwordEncoder;
     private final TeacherRepository teacherRepository;
-    private final TokenRepository tokenRepository;
-    private final TokenMapper tokenMapper;
     private final TeacherMapper teacherMapper;
 
     public void insertInitialTeachers() {
@@ -32,12 +25,5 @@ public class TeacherService {
                 teacherRepository.save(teacher);
             }
         }
-    }
-
-    public TokenDto generateToken() {
-        String generatedString = RandomString.make(128);
-        Token token = Token.builder().token(generatedString).build();
-        token = tokenRepository.save(token);
-        return tokenMapper.daoToDto(token);
     }
 }
