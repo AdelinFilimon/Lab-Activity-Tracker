15bce4bf203ec81980e509c27909b6c3c6f6bcae
Adelin <filimon.adelin24@gmail.com>
2021-04-11 10:01:52 +0300

    Remove default behaviour

--- src/main/java/com/gmail/filimon24/adelin/labactivitytracker/business/service/mapper/Mapper.java
+++ src/main/java/com/gmail/filimon24/adelin/labactivitytracker/business/service/mapper/Mapper.java
@@ -1,14 +1,8 @@
 package com.gmail.filimon24.adelin.labactivitytracker.business.service.mapper;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 public interface Mapper<DataAccessObject, DataTransferObject> {
 
-    default DataTransferObject daoToDto(DataAccessObject dataAccessObject) {
-        throw new NotImplementedException();
-    }
+    DataTransferObject daoToDto(DataAccessObject dataAccessObject);
 
-    default DataAccessObject dtoToDao(DataTransferObject dataTransferObject) {
-        throw new NotImplementedException();
-    }
+    DataAccessObject dtoToDao(DataTransferObject dataTransferObject);
 }
